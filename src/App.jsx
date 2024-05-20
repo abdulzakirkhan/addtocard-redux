92f6c54597a841cb3b22ffed8e383d561546821b
Abdul Zakir <abdulzakir632@gmail.com>
2024-05-20 10:40:41 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import { Card, Col, Container, Row } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <Container className="mt-5">
+      <Row className="g-3">
+       
+      </Row>
+    </Container>
+  )
+}
+
+export default App
