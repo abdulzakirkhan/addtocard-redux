be6b4ae09050c255654635175ef9a809ffa96371
Abdul Zakir <abdulzakir632@gmail.com>
2024-05-20 12:01:57 +0500

    folder Structur for reducers , actions , containers

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
-import './App.css'
-import { Card, Col, Container, Row } from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import { Card, Col, Container, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Container className="mt-5">
-      <Row className="g-3">
-       
-      </Row>
+      <Row className="g-3"></Row>
     </Container>
-  )
+  );
 }
 
-export default App
+export default App;
